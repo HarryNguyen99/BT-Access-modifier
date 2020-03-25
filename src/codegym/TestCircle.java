4c57778c5fd3b74ca800a7a12a4fb7ae88e17234
Harry Nguyen <50328363+HarryNguyen99@users.noreply.github.com>
2020-03-25 20:41:24 +0700

    Accesss Modifier

---
+++ src/codegym/TestCircle.java
@@ -0,0 +1,9 @@
+package codegym;
+
+public class TestCircle {
+    public static void main(String[] args) {
+        Circle circle = new Circle(10);
+        System.out.println("Radius: " + circle.getRadius());
+        System.out.println("Area: " + circle.getArea());
+    }
+}
